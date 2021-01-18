fd25b9b15a12405b62ba3862a0730b25b311955b
Caleb Cushing <xenoterracide@gmail.com>
2021-01-18 17:52:29 -0600

    update dot files

--- .config/my/rc.sh
+++ .config/my/rc.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-eval `keychain --eval --agents ssh --inherit any id_rsa`
+eval `keychain --eval --inherit any id_rsa`
 
 if command -v perl >/dev/null 2>&1; then
   if perl -Mlocal::lib -e 1 >/dev/null 2>&1; then
