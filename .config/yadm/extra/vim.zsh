53a7ab5ce9ea1f8d87e8ef0f5c26e7821d92bbc3
Caleb Cushing <xenoterracide@gmail.com>
2021-07-16 01:39:15 -0400

    update dot files

--- .config/yadm/extra/vim.zsh
+++ .config/yadm/extra/vim.zsh
@@ -1,23 +1,22 @@
 VIM_COLORDIR="$HOME/.vim/colors"
 VIM_PLUGDIR="$HOME/.vim/pack/local/start"
 VIM_AFTER_SYNTAX="$HOME/.vim/after/syntax"
-VIM_EDITORCONFIG="editorconfig-vim"
 
 mkdir -p $VIM_PLUGDIR
 mkdir -p $VIM_AFTER_SYNTAX
 
-zinit ice wait
+zinit ice wait cloneonly
 zinit light \
   atClone"cp -a * $VIM_PLUGDIR" \
   atPull"cp -a * $VIM_PLUGDIR" \
-  for editorconfig/$VIM_EDITORCONFIG
+  for editorconfig/editorconfig-vim
 
-zinit ice wait \
+zinit ice wait cloneonly \
   atClone"cp colors/desertlink.vim $VIM_COLORDIR" \
   atPull"cp colors/desertlink.vim $VIM_COLORDIR"
 zinit light toupeira/vim-desertink
 
-zinit ice wait \
+zinit ice wait cloneonly \
   atClone"cp after/syntax/zsh.vim $VIM_COLORDIR" \
   atPull"cp after/syntax/zsh.vim $VIM_COLORDIR"
 zinit light zinit-zsh/zinit-vim-syntax
