3e8057306eaf0d0bfe8e3253da83721f1cc4a7f5
Caleb Cushing <xenoterracide@gmail.com>
2021-07-23 14:35:52 -0400

    update dot files

--- .config/yadm/extra/alias.sh
+++ .config/yadm/extra/alias.sh
@@ -6,6 +6,8 @@ alias vim="vim -Xp"
 alias brix="java -jar ~/.local/share/brix/brix.jar"
 alias clip="xclip -selection clipboard -in"
 alias bfg="java -jar ~/.local/share/bfg.jar"
+alias logs="journalctl --output cat --boot 0"
+alias dmesg=dmesg --color=always | less
 
 if command -v bat >/dev/null 2>&1; then
   alias cat="bat --paging=never --plain"
@@ -17,8 +19,5 @@ fi
 
 if command -v plocate >/dev/null 2>&1; then
   alias locate="plocate"
-fi
-if command -v journalctl >/dev/null 2>&1; then
-  alias logs="journalctl -o cat -b 1"
 fi
 ######
