d93ac151318703f6f161109d42b44d4fd0c3788d
Caleb Cushing <xenoterracide@gmail.com>
2022-11-18 15:38:54 -0500

    update dot files

--- .config/yadm/extra/zshrc.os.Darwin,e.zsh
+++ .config/yadm/extra/zshrc.os.Darwin,e.zsh
@@ -3,5 +3,3 @@ PATH="$(brew --prefix findutils)/libexec/gnubin:$PATH"
 PATH="$(brew --prefix grep)/libexec/gnubin:$PATH"
 PATH="$(brew --prefix gnu-sed)/libexec/gnubin:$PATH"
 zi wait has"brew" atload"(( $+commands[brew] )) && . $(brew --prefix asdf)/libexec/asdf.sh" for z-shell/null
-
-if [ -d $(brew --prefix
