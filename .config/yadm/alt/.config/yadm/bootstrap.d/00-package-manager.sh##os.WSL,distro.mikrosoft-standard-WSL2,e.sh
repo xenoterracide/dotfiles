7f9983bc6bdc6760ee0c4649f6ba456ff5142c5e
Caleb Cushing <xenoterracide@gmail.com>
2023-03-24 05:07:47 -0400

    update dot files

---
+++ .config/yadm/alt/.config/yadm/bootstrap.d/00-package-manager.sh##os.WSL,distro.mikrosoft-standard-WSL2,e.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+sudo pacman -S \
+   aspell-en \
+   awesome-terminal-fonts \
+   bitwarden-cli \
+   diff-so-fancy \
+   gpg-tui \
+   jq \
+   mtr \
+   nano \
+   otf-fira-mono \
+   otf-fira-sans \
+   parallel \
+   procs \
+   tokei \
+   traceroute \
+   vi \
+   vim-spell-en \
+   whois \
+   yadm \
+   zoxide \
+   asdf-vm \
+   kalker \
+   otf-symbola \
+   bfg \
+   github-cli
