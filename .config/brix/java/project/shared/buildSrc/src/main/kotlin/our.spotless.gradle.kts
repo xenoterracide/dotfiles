3f8dbe52cc09cd102ba443738233b3e9c18ebb60
Caleb Cushing <xenoterracide@gmail.com>
2023-08-16 11:34:37 -0400

    update dot files

--- .config/brix/java/project/shared/buildSrc/src/main/kotlin/our.spotless.gradle.kts
+++ .config/brix/java/project/shared/buildSrc/src/main/kotlin/our.spotless.gradle.kts
@@ -14,7 +14,7 @@ spotless {
   java {
     licenseHeader(
       """
-      /**
+      /*
 
       $copyright
 
@@ -30,7 +30,7 @@ spotless {
       See the License for the specific language governing permissions and
       limitations under the License.
 
-      **/
+      */
       """.trimIndent().normaliseLineSeparators()
     )
   }
