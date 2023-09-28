ebfd74f8847f81b056377e9fa82b98286bc14610
Caleb Cushing <xenoterracide@gmail.com>
2023-09-28 16:00:50 -0400

    update dot files

--- .config/brix/java/project/shared/module/game/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.xenoterracide.ai.wh40k.game {
-  requires transitive java.measure;
-  requires systems.uom.common;
-  requires tech.units.indriya;
-  requires java.base;
-  requires org.inferred.freebuilder;
-}
