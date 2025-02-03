2befbaa406176dd1f78c6f84ba8ef038834a885a
Caleb Cushing <caleb.cushing@gmail.com>
2025-02-03 16:41:13 -0500

    update dot files

--- .config/yadm/alt/.profile##os.Darwin,e.zsh
+++ .config/yadm/alt/.profile##os.Darwin,e.zsh
@@ -1,10 +1,12 @@
+#!/bin/sh
 # SPDX-FileCopyrightText: Copyright © 2020 - 2025 Caleb Cushing
 #
 # SPDX-License-Identifier: MIT
 
 if [ -f /opt/homebrew/bin/brew ]; then
   eval "$(/opt/homebrew/bin/brew shellenv)"
-  if [ -f $(brew --prefix asdf)/libexec/asdf.sh ]; then
-    . $(brew --prefix asdf)/libexec/asdf.sh
+  if [ -f "$(brew --prefix asdf)/libexec/asdf.sh" ]; then
+    . "$(brew --prefix asdf)/libexec/asdf.sh"
   fi
-if
+fi
+
