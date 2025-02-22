98461a561a288cd221ae98296850d36df16ba2c9
Caleb Cushing <caleb.cushing@gmail.com>
2025-02-22 15:45:47 -0500

    update dot files

--- .config/yadm/alt/.profile##os.Linux,e.sh
+++ .config/yadm/alt/.profile##os.Linux,e.sh
@@ -1,17 +1,9 @@
 # SPDX-FileCopyrightText: Copyright © 2020 - 2024 Caleb Cushing
 #
 # SPDX-License-Identifier: MIT
+# shellcheck shell=sh
 
-if [ -f /opt/asdf-vm/asdf.sh ]; then
-  . /opt/asdf-vm/asdf.sh
-  if [ -d $HOME/.asdf/plugins/java ]; then
-    if [ $0 == "/bin/sh" || $SHELL == "/bin/bash" ]; then
-      . $HOME/.asdf/plugins/java/set-java-home.bash
-    elif [ $SHELL == "/bin/zsh" ]; then
-      . $HOME/.asdf/plugins/java/set-java-home.zsh
-    fi
-  fi
-fi
+export PATH="${ASDF_DATA_DIR:-$HOME/.asdf}/shims:$PATH"
 
 # Added by Toolbox App
 export PATH="$PATH:/home/xeno/.local/share/JetBrains/Toolbox/scripts"
